d7063f9c5a2d7889f5e5d850685df8b1c179f763
Ariel <verdejo.ariel@gmail.com>
2019-07-16 10:21:29 -0300

    Sumo archivo credentials.h .

---
+++ NodeMCU-app/Sketchs-Arduino/NodeMCU-DHT11-mosquitto/credentials.h
@@ -0,0 +1,12 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define AP_1 "Aca_tu_Nombre_de_Red"
+#define AP_1_PASS "Aca_tu_Password"
+
+#define MY_FINGERPRINT "Tu_Fingerprint"
+
+// Fingerprint of the broker CA
+// openssl x509 -in  mqttserver.crt -sha1 -noout -fingerprint
+
+#endif
